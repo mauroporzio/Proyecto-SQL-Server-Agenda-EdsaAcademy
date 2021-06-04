0ed421ba5520533b04d9552747a582d7ce89a11d
mauroporzio <mauroporzio02@gmail.com>
2021-06-04 15:59:01 -0300

    Push tabla Pais y Area
    Se agregaron las tablas de pías y área que serán utilizadas para la carga de los drop down en la UI de usuario.

--- SQL Server Scripts Agenda EdsaAcademy/SQLQuery_Tables.sql
+++ SQL Server Scripts Agenda EdsaAcademy/SQLQuery_Tables.sql
@@ -46,8 +46,53 @@ CREATE TABLE AgendaContactos_Historica
     FechaModifReg DATETIME NULL
 )
 
+
+
+CREATE TABLE Pais -- TABLA QUE CONTIENE LOS VALORES DE LOS PAISES QUE ESTARN DISPONIBLES EN EL DROP DOWN DENTRO DEL UI
+(
+	IdPais INT IDENTITY(1,1),
+	NombrePais VARCHAR (20) NOT NULL
+)
+
+INSERT INTO [dbo].[Pais] (NombrePais) VALUES 
+
+	('Argentina'),
+	('Bolivia'),	
+	('Brasil'),	
+	('Chile'),
+	('Colombia'),	
+	('Ecuador'),	
+	('Paraguay'),	
+	('Perú'),	
+	('Uruguay'),	
+	('Venezuela'),
+	('Estados Unidos'),
+	('Alemania'),
+	('España'),
+	('Francia'),
+	('Reino Unido'),
+	('China'),
+	('Japon')
+
+SELECT * FROM Pais
+
+
+CREATE TABLE Area -- TABLA QUE CONTIENE LOS VALORES DE LAS AREAS QUE ESTARN DISPONIBLES EN EL DROP DOWN DENTRO DEL UI
+(
+	IdArea INT IDENTITY(1,1),
+	NombreArea VARCHAR (20) NOT NULL
+)
+
+INSERT INTO [dbo].[Area] (NombreArea) VALUES ('Marketing'), ('Finanzas'), ('Finanzas'), ('RRHH'), ('Operaciones')
+
+SELECT * FROM AREA
+
 --Borrar Tablas
 
 DROP TABLE AgendaContactos
 
 DROP TABLE AgendaContactos_Historica
+
+DROP TABLE Pais
+
+DROP TABLE Area
